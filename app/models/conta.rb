9d387e968d4d7e082b692ac3544061bdfc3ceba7
EmanuelNog <emanuel.nog0@gmail.com>
2025-03-05 04:13:47 -0300

    fix cascade destroy

--- app/models/conta.rb
+++ app/models/conta.rb
@@ -1,4 +1,4 @@
 class Conta < ApplicationRecord
   belongs_to :pessoa, foreign_key: 'Pessoa_id'
-  has_many :transacoes, class_name: 'Transacao'
+  has_many :transacoes, class_name: 'Transacao', foreign_key: 'Conta_id', dependent: :destroy
 end
