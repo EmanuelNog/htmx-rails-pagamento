c228918d2036205bcf83149a997520ab22cb1c69
EmanuelNog <emanuel.nog0@gmail.com>
2025-03-05 04:25:45 -0300

    clean application.css

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,7 +1,3 @@
 class Pessoa < ApplicationRecord
   has_many :contas, foreign_key: 'Pessoa_id', dependent: :destroy
-  
-  # When a pessoa is deleted:
-  # 1. All their contas will be deleted (due to dependent: :destroy above)
-  # 2. All transacoes of those contas will be deleted (due to the Conta model's has_many below)
 end
